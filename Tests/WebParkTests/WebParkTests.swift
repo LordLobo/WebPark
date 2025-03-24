8f5c4eca2c1d241ea3f869650633a84826b21963
Dan Giralte <dan.giralte@gmail.com>
2025-03-24 06:44:59 -0500

    correct implementation

--- Tests/WebParkTests/WebParkTests.swift
+++ Tests/WebParkTests/WebParkTests.swift
@@ -10,7 +10,9 @@ import XCTest
 
 final class WebParkTests: XCTestCase {
     func test__createRequest__given_valid_request__returns_request() throws {
-        let sut = Implementation(baseURL: "http://google.com/", token: "", urlSession: BuildGETURLSession())
+        let sut = Implementation(tokenService: testTokenService(),
+                                 baseURL: "http://google.com/",
+                                 urlSession: BuildGETURLSession())
         
         let result = try sut.createRequest("GET", endpoint: "foo")
         
